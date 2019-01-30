b5c454597221441b900523ff2e717f58cf1ae3fd
John Celoria <john.celoria@q2ebanking.com>
2019-01-30 12:24:54 -0600

    fix regex

--- configure-apache.sh
+++ configure-apache.sh
@@ -12,8 +12,8 @@ for i in /usr/lib/libxml2.so*; do
 done
 
 # update httpd.conf
-sed -i 's/^#\(ServerRoot\).*/\1 /etc/apache2/g' /etc/apache2/httpd.conf
-sed -i 's/^#\(ServerName\).*/\1 ${SERVER_NAME}/g' /etc/apache2/httpd.conf
+sed -i 's@^#*\(ServerRoot\).*@\1 /etc/apache2@g' /etc/apache2/httpd.conf
+sed -i 's@^#\(ServerName\).*@\1 ${SERVER_NAME}@g' /etc/apache2/httpd.conf
 
 # move default configuration files out of the way
 mkdir /tmp/defaults && mv /etc/apache2/conf.d/* /tmp/defaults
