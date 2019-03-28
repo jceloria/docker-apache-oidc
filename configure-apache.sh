5eac2717e1e737df2678e39ee4ec716d97300132
John Celoria <john.celoria@q2ebanking.com>
2019-03-28 10:50:10 -0500

    organize defaults, update logging

--- configure-apache.sh
+++ configure-apache.sh
@@ -1,30 +1,35 @@
 #!/bin/sh
-
 set -xe
 
-mv modules.d /etc/apache2 && cd /etc/apache2
+cd /tmp/build
+
+mkdir -p /etc/apache2/conf.d
+mv modules.d /etc/apache2/conf.d
+
+cd /etc/apache2
 
 # fix paths
 mkdir -p /run/apache2
-ln -s ../../usr/lib/apache2 modules
-ln -s ../../var/log/apache2 logs
-ln -s /run/apache2 run
+ln -sf ../../usr/lib/apache2 modules
+ln -sf ../../var/log/apache2 logs
+ln -sf /run/apache2 run
 
-# move default configuration files out of the way
-mkdir -p /tmp/defaults && mv httpd.conf modules.d conf.d/* /etc/php7 /tmp/defaults
-chown -R root:root /tmp/defaults
+# save defaults for later
+for src in ${PWD}/httpd.conf ${PWD}/conf.d /etc/php7; do
+    dst=/tmp/defaults${src%/*}; mkdir -p ${dst}; mv ${src} ${dst}
+done && chown -R root:root /tmp/defaults
 
 # load modules from included directory
 sed -n '/foo_module/{p;:a;N;/IfModule unixd_module/!ba;s/.*\n/#\nInclude conf.d\/modules.d\/\*.conf\n\n/};p' \
-    /tmp/defaults/httpd.conf > httpd.conf
+    /tmp/defaults${PWD}/httpd.conf > httpd.conf
 
-# update minimum required configuration options
+# update configuration
 sed -i 's@^#*\(ServerRoot\).*@\1 /etc/apache2@g' httpd.conf
 sed -i 's@^#\(ServerName\).*@\1 ${SERVER_NAME}@g' httpd.conf
 sed -i 's@^#*\(ServerAdmin\).*@\1 ${SERVER_ADMIN}@g' httpd.conf
 sed -i 's@/var/www/localhost/htdocs@${DOCUMENT_ROOT}@g' httpd.conf
-sed -i 's@^#*\(Errorlog\).*@\1 /dev/stderr@g' httpd.conf
-sed -i 's@^#*\(Transferlog\).*@\1 /dev/stdout@g' httpd.conf
+sed -i 's@^\(ErrorLog\).*@\1 /dev/stderr@g' httpd.conf
+sed -i '/.*#/!s@\(CustomLog\).*@\1 /dev/stdout combined@g' httpd.conf
 
 # generate a snakeoil certificate in case it's needed
 openssl req -newkey rsa:4096 -days 365 -nodes -x509 \
