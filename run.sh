81782a3635fd0a9ecf71aa2ec140753c70a817bc
John Celoria <john.celoria@q2ebanking.com>
2019-03-28 10:48:12 -0500

    tar copy instead of rsync

--- run.sh
+++ run.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
-
 set -xe
 
+# set variable dependencies
 export SERVER_NAME="${SERVER_NAME:=localhost}"
 export SERVER_ADMIN="${SERVER_ADMIN:=root@localhost}"
 export DOCUMENT_ROOT="${DOCUMENT_ROOT:=/var/www/localhost/htdocs}"
 
-rsync -av --ignore-existing /tmp/defaults/modules.d /etc/apache2/conf.d
-rsync -av --ignore-existing /tmp/defaults/php7 /etc
-rsync -av --remove-source-files --ignore-existing /tmp/defaults /etc/apache2/conf.d
+# copy apache defaults if they don't already exist
+find /etc/apache2/conf.d/* >/dev/null 2>&- || {
+    src=/tmp/defaults/etc/apache2 dst=/etc/apache2
+    (cd ${src} && tar -cf - .) | (cd ${dst} && tar --skip-old-files -xf -)
+}
 
-find /etc/apache2/conf.d /tmp/defaults -depth -type d -exec rmdir {} \; 2>/dev/null
+# copy php defaults if they don't already exist
+find /etc/php7/* >/dev/null 2>&- || {
+    src=/tmp/defaults/etc dst=/etc
+    (cd ${src} && tar -cf - php7) | (cd ${dst} && tar --skip-old-files -xf -)
+}
 
+# clean up
+rm -rf /tmp/defaults
+
+# execute a custom script
 if [[ -f "${CUSTOM_SCRIPT}" ]]; then
     echo "A custom script has been found: ${CUSTOM_SCRIPT}"
     . ${CUSTOM_SCRIPT}
